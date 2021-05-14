610c8836fbb8f8688b2136cb378a957761f58945
Joe Fatani <jucepig@gmail.com>
2021-05-14 09:16:59 -0600

    created seed for tables and set up massive on server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
+const massive = require('massive')
 const express = require('express'),
       userCtrl = require('./controllers/user'),
       postCtrl = require('./controllers/posts')
-
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 const app = express();
 
 app.use(express.json());
+
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {rejectUnauthorized: false}
+}).then(db => {
+  console.log('Database Connected')
+  app.set('db', db)
+  app.listen(SERVER_PORT, _ => console.log(`running on ${SERVER_PORT}`));
+})
 
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
@@ -20,4 +31,4 @@ app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
 
-app.listen(4000, _ => console.log(`running on ${4000}`));
+app.listen(SERVER_PORT, _ => console.log(`running on ${SERVER_PORT}`));
